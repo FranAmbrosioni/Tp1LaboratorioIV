7f6a42046f968bcb7390aaa3b5700d2d5b37a05a
unknown <ambrosionifrancisco@gmail.com>
2022-10-01 10:06:54 -0300

    finish CRUD

---
+++ Migrations/20221001120228_inicio2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebTp1LaboratorioIV.Migrations
+{
+    public partial class inicio2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
